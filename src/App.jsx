bc45a95e1451dd450b783fe15c444f82a425869d
paresh2421 <paresh2411@gmail.com>
2025-06-14 14:32:23 +0530

    Third commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ const App = () => {
   const [activeId, setActiveId] = useState(null)
 
   const toggleQuestion=(id)=>{
-    
-    setActiveId(id)
+    const newActiveId = id === activeId ? null : id;
+    setActiveId(newActiveId)
   }
 
   return (
--- src/SingleQuestion.jsx
+++ src/SingleQuestion.jsx
@@ -1,19 +1,20 @@
 import { useState } from 'react'
 import {AiOutlineMinus, AiOutlinePlus} from 'react-icons/ai'
 
-const SingleQuestion = ({ title, info, activeId, toggleQuestion }) => {
+const SingleQuestion = ({ id, title, info, activeId, toggleQuestion }) => {
     const [showInfo, setShowInfo] = useState(false)
+    const isActive = id === activeId
   return (
         <article className="question">
             <header>
                 <h5>
                     {title}
                 </h5>
-                <button className="question-btn" onClick={()=>setShowInfo(!showInfo)}>
+                <button className="question-btn" onClick={()=>toggleQuestion(id)}>
                     {showInfo ? <AiOutlineMinus />:<AiOutlinePlus /> }
                 </button>
             </header>
-            {showInfo && <p>{info}</p>}
+            {isActive && <p>{info}</p>}
         </article>
   )
 }
